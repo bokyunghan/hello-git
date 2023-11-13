f2fd87c622b7601c10bb604964e1e140dbfac4d5
bokyunghan <jsbkbr98@naver.com>
2023-11-13 11:37:46 +0900

    원숭이 말함 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,15 +1,14 @@
 package com.sh.app;
 
-import com.sh.app.animal.Capibara;
-import com.sh.app.animal.Cat;
-import com.sh.app.animal.Dog;
+import com.sh.app.animal.Monkey;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("HELLO WORLD");
 		System.out.println("HELLO GIT");
-	
+		
+	new Monkey().talk();
 
 	}
 
---
+++ src/com/sh/app/animal/Monkey.java
@@ -0,0 +1,7 @@
+package com.sh.app.animal;
+
+public class Monkey {
+public void talk() {
+	System.out.println("원숭이 말해!");
+}
+}
