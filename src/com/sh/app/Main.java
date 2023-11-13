95d8e84332b8c461855095fb3709fc7d4ba4c8a5
bokyunghan <jsbkbr98@naver.com>
2023-11-13 11:48:00 +0900

    Main 동물기능 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,5 +1,6 @@
 package com.sh.app;
 
+import com.sh.app.animal.Bird;
 import com.sh.app.animal.Capibara;
 import com.sh.app.animal.Cat;
 import com.sh.app.animal.Dog;
@@ -10,7 +11,11 @@ public class Main {
 		System.out.println("HELLO WORLD");
 		System.out.println("HELLO GIT");
 	
-
+		System.out.println("동물 기능 시작~");
+		new Bird().fly();
+		new Capibara().cry();
+		new Cat().jump();
+		new Dog().bark();
 	}
 
 }
